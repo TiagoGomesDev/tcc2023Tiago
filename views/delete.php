511487f4c6ac09153a9785f10a1d1e4d5d898823
Tiago Gomes <67073846+TiagoGomesDev@users.noreply.github.com>
2023-10-13 21:53:23 -0300

    Delete views directory

--- views/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id = $_GET['id'];
-$conexion=mysqli_connect("localhost","root","","gcred"); 
-// $query = mysqli_query($conexion,"DELETE FROM user WHERE id = '$id'");
-$query = mysqli_query($conexion,"DELETE FROM vendas WHERE id_vendas = '$id_vendas'");
-
-header ('Location: prueba.php?m=1');
